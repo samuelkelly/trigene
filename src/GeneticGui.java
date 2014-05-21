d11045ecdbf746cff5b1305a494353ba5946acf5
quixoticdicker <cdicker@lclark.edu>
2014-05-21 15:55:24 -0700

    panel now shows up and displays a test image and a black square where the genetic image will show up

--- src/GeneticGui.java
+++ src/GeneticGui.java
@@ -1,12 +1,64 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
-public class GeneticGui extends JFrame {
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-	/**
-	 * @param args
-	 */
+
+public class GeneticGui extends JPanel {
+
+	BufferedImage imageToBecome;
+	
+	public GeneticGui()
+	{
+		try {
+			File file = new File("testImage.png");
+			imageToBecome  = ImageIO.read(file);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+		
+		
+	}
+	
+	public void paintComponent(Graphics g)
+	{
+		g.drawImage(imageToBecome, 10, 10, 480, 480, null);
+		
+		g.setColor(Color.black);
+		g.fillRect(510, 10, 480, 480);		
+	}
+	
+	public Dimension getMaximumSize() {
+		return new Dimension(1000, 500);
+	}
+	
+	public Dimension getMinimumSize() {
+		return new Dimension(1000, 500);
+	}
+	
+	public Dimension getPreferredSize() {
+		return new Dimension(1000, 500);
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				JFrame frame = new JFrame();
+				frame.setTitle("Genetic Algorithm Prototype: Triangle Drawer");
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.setVisible(true);
+				frame.add(new GeneticGui());
+				frame.pack();
+			}
+		});
 	}
 
 }
