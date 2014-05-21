b8e3d43343dc35d8d86ccebc385df0f335e1d996
johnsibandze <johnsibandze@lclark.edu>
2014-05-21 16:06:59 -0700

    just changed some comments :)

--- src/CompareImages.java
+++ src/CompareImages.java
@@ -1,7 +1,6 @@
 import java.awt.Color;
 import java.awt.image.BufferedImage;
 
-
 public class CompareImages {
 
 	public double compareImages(BufferedImage imageA, BufferedImage imageB) {
@@ -10,18 +9,19 @@ public class CompareImages {
 
 		// the fitness value
 		double fitness = 0;
-		
-		// for all the pixels
+
+		// run through all the pixels
 		for (int i = 0; i < height; i++) {
 			for (int j = 0; j < width; j++) {
+				// for image a
 				int imageARGB = imageA.getRGB(i, j);
-				int imageBRGB = imageB.getRGB(i, j);
-
 				Color colorA = new Color(imageARGB);
 				double aRed = colorA.getRed();
 				double aGreen = colorA.getGreen();
 				double aBlue = colorA.getBlue();
 
+				// for image b
+				int imageBRGB = imageB.getRGB(i, j);
 				Color colorB = new Color(imageBRGB);
 				double bRed = colorB.getRed();
 				double bGreen = colorB.getGreen();
