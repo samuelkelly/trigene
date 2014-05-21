65bbf5ae3fea00302e481cc1560f9b53fbee3f7e
johnsibandze <johnsibandze@lclark.edu>
2014-05-21 16:21:36 -0700

    renamed variables

--- src/CompareImages.java
+++ src/CompareImages.java
@@ -1,7 +1,6 @@
 import java.awt.Color;
 import java.awt.image.BufferedImage;
 
-
 public class CompareImages {
 
 	public double compareImages(BufferedImage imageA, BufferedImage imageB) {
@@ -10,19 +9,20 @@ public class CompareImages {
 
 		// the fitness value
 		double fitness = 0;
-		
+
 		// for all the pixels
 		for (int i = 0; i < height; i++) {
 			for (int j = 0; j < width; j++) {
-				int imageARGB = imageA.getRGB(i, j);
-				int imageBRGB = imageB.getRGB(i, j);
-
-				Color colorA = new Color(imageARGB);
+				// for image a
+				int imageARgb = imageA.getRGB(i, j);
+				Color colorA = new Color(imageARgb);
 				double aRed = colorA.getRed();
 				double aGreen = colorA.getGreen();
 				double aBlue = colorA.getBlue();
 
-				Color colorB = new Color(imageBRGB);
+				// for image b
+				int imageBRgb = imageB.getRGB(i, j);
+				Color colorB = new Color(imageBRgb);
 				double bRed = colorB.getRed();
 				double bGreen = colorB.getGreen();
 				double bBlue = colorB.getBlue();
